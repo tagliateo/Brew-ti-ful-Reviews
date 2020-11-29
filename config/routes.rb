b4b21240a65bded7fe32de9813d118d35ce8f134
Teodoro Garcia <tedtke514@gmail.com>
2020-11-28 19:11:33 -0600

    g sessions controller, views, routes set home and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'sessions#home'
 
   get '/signup', to: 'users#new'
 
   resources :coffees
   resources :users, only: [:new, :create, :show]
   resources :reviews
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
