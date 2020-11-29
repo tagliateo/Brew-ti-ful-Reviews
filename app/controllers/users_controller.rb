3bc3bf7a34f698bffcb9f30ffd6abb064ae1d88c
Teodoro Garcia <tedtke514@gmail.com>
2020-11-28 18:32:00 -0600

    implemented users:create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    # byebug
+    @user = User.new(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :profile_img)
   end
 end
