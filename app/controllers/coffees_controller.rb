8fababd7cd5c8ca2b1baebc831fe718a84f6247c
Teodoro Garcia <tedtke514@gmail.com>
2020-11-29 17:40:06 -0600

    added C for CRUD in Coffee

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -1,2 +1,25 @@
 class CoffeesController < ApplicationController
+  include SessionsHelper
+
+  def new
+    @coffee = Coffee.new
+  end
+
+  def create
+    @coffee = current_user.coffees.build(beer_params)
+    if @coffee.save
+      redirect_to coffee_path(@coffee)
+    else
+      render :new
+    end 
+  end
+  private
+
+  def find_coffee
+    @coffee = Coffee.find_by_id(params[:id])
+  end
+
+  def coffee_params
+    params.require(:coffee).permit(:title, :roaster, :description, :roast_type)
+  end
 end
