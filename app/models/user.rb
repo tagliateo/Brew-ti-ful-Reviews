c8aac94a4250000f5a146d3a2ec5e16cf103366d
Teodoro Garcia <tedtke514@gmail.com>
2020-12-02 19:14:08 -0600

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :coffees, through: :reviews
   # accepts_nested_attributes_for :coffee
   has_secure_password
+
+  validates :name, presence: true
+  validates :email, format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
+  validates :profile_img, presence: true
 end
