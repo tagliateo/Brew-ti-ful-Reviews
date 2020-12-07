ae31628cba57bda37c6d92bcebecf4e6885f01ae
Teodoro Garcia <tedtke514@gmail.com>
2020-12-07 15:10:58 -0600

    updated oauth login method in sessions controller to display profile img
    updated user show page to display profile img if there is one

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,14 +21,15 @@ class SessionsController < ApplicationController
   end
 
   def google_login
-    user_email = request.env['omniauth.auth']['info']['email']
-    user_name = request.env['omniauth.auth']['info']['name']
-
-    @user = User.find_or_create_by(email: user_email) do |user|
-      user.name = user_name
-      user.password = SecureRandom.hex
-    end
-    session[:user_id] = @user.id
-    redirect_to user_path(@user)
-end
+   user_email = request.env['omniauth.auth']['info']['email']
+   user_name = request.env['omniauth.auth']['info']['name']
+   user_image = request.env['omniauth.auth']['info']['image']
+   @user = User.find_or_create_by(email: user_email) do |user|
+     user.name = user_name
+     user.password = SecureRandom.hex
+     user.profile_img = user_image
+   end
+   session[:user_id] = @user.id
+   redirect_to user_path(@user)
+ end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, uniqueness: true
-  validates :email, format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
+  # validates :email, format: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
   validates :email, presence: true, allow_nil: true
   validates :profile_img, presence: true
 end
