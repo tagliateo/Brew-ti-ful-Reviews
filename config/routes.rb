886c1bee64eb2b42174cc4fa4f01e5a152d2b00b
Teodoro Garcia <tedtke514@gmail.com>
2020-12-08 09:41:11 -0600

    added User info to the show for reviews and coffees index updated routes to fix issue with logout button in the navbar partial

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   # logout
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
 
   get '/auth/google_oauth2/callback', to: "sessions#google_login"
 
