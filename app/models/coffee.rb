3d7b60f74c3f7aea60abcbc2f738a8de4445a9f8
Teodoro Garcia <tedtke514@gmail.com>
2020-12-20 21:25:19 -0600

    corrected validations to allow more user inputs

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -5,8 +5,8 @@ class Coffee < ApplicationRecord
   has_many :users, through: :reviews
 
   validates :title, :brand, :roast_level, :description, :caffeine_content, presence: true
-  validates :roast_level, inclusion: { in: %w(light medium dark), message: "%{value} is not a valid roast level" }
-  validates :caffeine_content, inclusion: { in: %w(low medium high ), message: "%{value} is not a valid roast level" }
+  validates :roast_level, inclusion: { in: %w(light medium dark Light Medium Dark), message: "%{value} is not a valid roast level" }
+  validates :caffeine_content, inclusion: { in: %w(low medium high Low Medium High), message: "%{value} is not a valid roast level" }
   validates :title, uniqueness: { message: "has already been added.", case_sensitive: false }
 
   scope :alphabetical, -> { order('name ASC') }
